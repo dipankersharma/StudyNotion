188583ad11c8ead3a976926e849847d201d1baed
Deeksha <deekshanand.pupri@gmail.com>
2025-03-11 14:48:53 +0530

    new team member added

--- src/component/core/Dashboard/AddCourse/PublishCourse.jsx
+++ src/component/core/Dashboard/AddCourse/PublishCourse.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 const PublishCourse = () => {
   return <div>
-    <h1 className="text-richblack-5">Helloo Everone</h1>
+    <h1 className="text-richblack-5">Helloo Everone</h1> 
+    <p className="text-richblack-5">Hy i am new member in this project </p>
   </div>;
 };
 
